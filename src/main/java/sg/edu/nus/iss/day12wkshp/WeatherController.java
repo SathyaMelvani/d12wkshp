30035d4389d892a37e01f499fc5b634ef30edd1a
Sathya <sathyamelwani@gmail.com>
2022-07-19 14:12:09 +0800

    weather

---
+++ src/main/java/sg/edu/nus/iss/day12wkshp/WeatherController.java
@@ -0,0 +1,35 @@
+import org.springframework.stereotype.Controller;
+import.org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller 
+@RequestMapping(path={"/weather"})
+public class WeatherController {
+
+    @GetMapping
+    public String weather(
+        @RequestParam(required=true) String city,
+        @RequestParam(name="units", defaultValue="kilometers") String units, 
+        Model model){
+
+            model.addAttribute(attributeName: "city", city);
+            model.addAttribute("units", units)
+            return "weather";
+
+            @GetMapping("{city}")
+            public String weather2(
+                @PathVariable(name="city", required=true) String city,
+                @RequestParam(name="units", defaultValue="kilometers") String units,
+                Model model
+            ){
+
+                model.addAttribute(attributeName: "city", city);
+                model.addAttribute("units", units)
+                return "weather";
+            }
+
+    }
+
+
+    
+}
