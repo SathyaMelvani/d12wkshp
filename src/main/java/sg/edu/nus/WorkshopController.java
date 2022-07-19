e952759b2f498db67087881ec9bb32620eb2b45d
Sathya <sathyamelwani@gmail.com>
2022-07-19 16:26:46 +0800

    workshop 2 completed

--- src/main/java/sg/edu/nus/WorkshopController.java
+++ src/main/java/sg/edu/nus/WorkshopController.java
@@ -2,7 +2,9 @@ import.org.springframework.stereotype.Controller;
 import.org.springframework.web.bind.annotation.*;
 import.org.springframework.ui.Model;
 import java.util.*;
-import java.lang.*;
+import java.lang.Integer;
+import java.lang.Math;
+
 
     @Controller
     public class WorkshopController {
@@ -12,8 +14,39 @@ import java.lang.*;
             return "workshop";
         }
 
+    @RequestMapping(path = { "/processNumber"})
+        public String testResults(
+            @RequestParam(name="inputNumber", defaultValue="1") Integer unit,
+            Model model
+        ){
 
-        
+            List<Integer> 1stInt = new ArrayList<>();
+            int loopValue = Integer.parseInt(unit.toString());
+
+            int loop = 1;
+            while (loop <= loopValue) {
+
+                int result = ((Math.random() * loopValue) + 1);
+
+                if(!1stInt.contains(Integer.valueOf(result))) {
+                    1stInt.add(Integer.valueOf(result));
+                    loop++;
+                }
+            }
+
+            for (int i = 0; i < 1stInt.size(); i++) {
+                System.out.print("==>" + 1stInt.get(i));
+                System.out.println();
+
+            }
+
+            model.addAttribute("numbers", 1stInt);
+            return "workshopresult";
+
+        }
+
+
+
 
     
 }
